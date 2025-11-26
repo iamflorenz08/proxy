00b0c73bfc892037f9bd6c8344e20b819e591c97
iamflorenz08 <86180087+iamflorenz08@users.noreply.github.com>
2025-11-26 12:33:36 +0800

    Update domain-templates.ts

--- src/config/domain-templates.ts
+++ src/config/domain-templates.ts
@@ -1252,7 +1252,7 @@ export const domainTemplates: DomainTemplate[] = [
   },
   //Stream me
   {
-    pattern: /\.stormyfield58\.pro$/i,
+    pattern: /\.windglow99\.pro$/i,
     headers: {
       'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0',
       'accept': '*/*',
